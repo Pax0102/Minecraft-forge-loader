a21c853b86b321da07429d9e49faa11c4706e4a1
Pax <mamaetata216@gmail.com>
2026-09-06 13:50:22 -0300

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Minecraft Server Manager
-chcp 65001 >nul
-
-powershell -NoProfile -ExecutionPolicy Bypass ^
-    -Command "[Console]::InputEncoding=[System.Text.UTF8Encoding]::new(); [Console]::OutputEncoding=[System.Text.UTF8Encoding]::new(); & '%~dp0Main.ps1'"
-
-pause
